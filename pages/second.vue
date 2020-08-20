1cfbe89a4450debfc4a4bed7dbdf510b65542a63
dr-r3d <abhi@appsbuddha.com>
2020-08-21 00:25:22 +0530

    adds link to ice page which can redirect url back to app

--- pages/second.vue
+++ pages/second.vue
@@ -1,5 +1,6 @@
 <template>
   <div style="text-align: center;">
     <h1>SECOND Page</h1>
+    <a href="https://app.internationalcyclingexecutives.com/.netlify/functions/ice?redirectUrl=https://nuxt-min-pwa.netlify.app">ICE</a>
   </div>
 </template>
