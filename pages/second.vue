727c2a3a6727b1748271a322deb2e85ac6be25a7
dr-r3d <abhi@appsbuddha.com>
2020-08-26 10:28:16 +0530

    change url

--- pages/second.vue
+++ pages/second.vue
@@ -1,6 +1,8 @@
 <template>
   <div style="text-align: center;">
     <h1>SECOND Page</h1>
-    <a href="https://app.internationalcyclingexecutives.com/.netlify/functions/ice?redirectUrl=https://nuxt-min-pwa.netlify.app">ICE</a>
+    <a href="https://app.internationalcyclingexecutives.com/.netlify/functions/ice?redirectUrl=https://radiant-anchorage-22925.herokuapp.com/">ICE</a>
+    <!-- <a href="https://app.internationalcyclingexecutives.com/.netlify/functions/ice?redirectUrl=http://192.168.1.19:8888">ICE</a> -->
+    <!-- <a href="https://app.internationalcyclingexecutives.com/.netlify/functions/ice?redirectUrl=https://nuxt-min-pwa.netlify.app">ICE</a> -->
   </div>
 </template>
