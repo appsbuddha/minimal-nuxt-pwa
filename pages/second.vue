96a76a0b65f6b4bbaa22f9cdff965df747d2060b
dr-r3d <abhi@appsbuddha.com>
2020-08-27 13:36:10 +0530

    add standalone to callback url

--- pages/second.vue
+++ pages/second.vue
@@ -1,8 +1,8 @@
 <template>
   <div style="text-align: center;">
     <h1>SECOND Page</h1>
-    <a href="https://app.internationalcyclingexecutives.com/.netlify/functions/ice?redirectUrl=https://radiant-anchorage-22925.herokuapp.com/">ICE</a>
+    <!-- <a href="https://app.internationalcyclingexecutives.com/.netlify/functions/ice?redirectUrl=https://radiant-anchorage-22925.herokuapp.com/">ICE</a> -->
     <!-- <a href="https://app.internationalcyclingexecutives.com/.netlify/functions/ice?redirectUrl=http://192.168.1.19:8888">ICE</a> -->
-    <!-- <a href="https://app.internationalcyclingexecutives.com/.netlify/functions/ice?redirectUrl=https://nuxt-min-pwa.netlify.app">ICE</a> -->
+    <a href="https://app.internationalcyclingexecutives.com/.netlify/functions/ice?redirectUrl=https%3A%2F%2Fnuxt-min-pwa.netlify.app&standalone=true">ICE</a>
   </div>
 </template>
